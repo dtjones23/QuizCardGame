cca3471b1e94945f5f8bd9c22515e67392a5d574
DeAngelo Jones <deangelojones0708@gmail.com>
2023-10-03 19:27:07 -0500

    created array in JS for questions to be answered for quiz

--- starter-state/assets/js/script.js
+++ starter-state/assets/js/script.js
@@ -1,61 +1,88 @@
-var preGameEl = document.getElementById("pre-game");
-var gameEl = document.getElementById("game");
-var gameEndEl = document.getElementById("game-end");
-var hiScoreEl = document.getElementById("hi-score");
+const quizQuestionsEl = [{
+    question: "Commonly used data types DO NOT include: ",
+    answer: 
+        [{text: "Strings", isCorrect : false },
+        {text: "Boolean", isCorrect: false },
+        {text: "alert", isCorrect: true },
+        {text: "Numbers", isCorrect: false },
+    ]
 
-var hiButtonEl = document.getElementById("hi");
-var backButtonEl = document.getElementById("back");
+},
+{
+    question: "The condition in an if/else statement is enclosed with _____. ",
+    answer: 
+        [{text: "quotes", isCorrect : false },
+        {text: "curly brackets", isCorrect: true },
+        {text: "parenthesis", isCorrect: false },
+        {text: "square brackets", isCorrect: false },
+    ]
+
+},
+{
+    question: "Arrays in JavaScript can be used to store_____. ",
+    answer: 
+        [{text: "numbers and strings", isCorrect : false },
+        {text: "other arrays", isCorrect: false },
+        {text: "booleans", isCorrect: false },
+        {text: "all of the above", isCorrect: true },
+    ]
+
+},
+{
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    answer: 
+        [{text: "commas", isCorrect : false },
+        {text: "curly brackets", isCorrect: false },
+        {text: "quotes", isCorrect: true },
+        {text: "parenthesis", isCorrect: false },
+    ]},
+    // List of the questions for quiz
+];
 
 
-hiButtonEl.addEventListener("click", viewHiScore);
-
-backButtonEl.addEventListener("click", function () {
-    document.location.reload();
-backButtonEl.addEventListener("click" , function () {
-    this.hidden
-})
-});
 
 
-function startGame() {
-    preGameEl.setAttribute("class", "is-inactive");
-    gameEl.setAttribute("class", "is-active");
-    }
+// const startQuizBtnEl = document.getElementById("start-quiz");
+// var preGameEl = document.getElementById("pre-game");
+// var gameEl = document.getElementById("game");
+// var gameEndEl = document.getElementById("game-end");
+// var hiScoreEl = document.getElementById("hi-score");
+
+// var answerButtonEl = document.getElementById("answerButton");
+// var startQuiz = document.getElementById("start-quiz");
+
+
+// var hiButtonEl = document.getElementById("hi");
+// var backButtonEl = document.getElementById("back");
+
+
+// hiButtonEl.addEventListener("click", viewHiScore);
+
+// backButtonEl.addEventListener("click", function () {
+//     document.location.reload();
+// });
+
+// startQuiz.addEventListener("click", startGame());
+
+// start the quiz game
+
+
+// function startGame() {
+//     preGameEl.setAttribute("class", "is-inactive");
+//     gameEl.setAttribute("class", "is-active");
+// }
     
     
 
-    // var timeInterval = setInterval(function (){
-     
-    //     // displays seconds remaining
-    //     if (quizTime > 1) {
-    //         gameEl.setAttribute("style","font-size:30px; position:absolute; top:0; right:10px;");
-    //         gameEl.textContent = quizTime + ' seconds remaining';
-    //         quizTime--;
-    //     }   else if(quizTime === 1) {
-    //         gameEl.textContent = quizTime + ' second remaining';
-    //         quizTime--;
-    //     }   else {
-    //         gameEl.textContent = 'end';
-    //         clearInterval(timeInterval);
-            
-    //         // endGame();
-    //         // starting endGame function
-    //     }
-        
-    // }, 1000);
 
-    
-    
+// function endGame() {
+//     game.setAttribute("class", "is-inactive");
+//     gameEnd.setAttribute("class", "is-active");
+// }
 
-
-function endGame() {
-    game.setAttribute("class", "is-inactive");
-    gameEnd.setAttribute("class", "is-active");
-}
-
-function viewHiScore() {
-    preGameEl.setAttribute("class", "is-inactive");
-    game.setAttribute("class", "is-inactive");
-    gameEndEl.setAttribute("class", "is-inactive");
-    hiScoreEl.setAttribute("class", "is-active");
-}
+// function viewHiScore() {
+//     preGameEl.setAttribute("class", "is-inactive");
+//     game.setAttribute("class", "is-inactive");
+//     gameEndEl.setAttribute("class", "is-inactive");
+//     hiScoreEl.setAttribute("class", "is-active");
+// }
